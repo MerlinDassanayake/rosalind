633ca89d9e27120072a806c5df196bc86f6a386f
Merlin Dassanayake <131916883+MerlinDassanayake@users.noreply.github.com>
2023-06-14 17:19:22 -0700

    episode 9

--- DNAToolkit.py
+++ DNAToolkit.py
@@ -1,4 +1,4 @@
-import collections
+from collections import *
 from structures import *
 
 #Check the sequence to make sure it is a DNA string
@@ -37,3 +37,20 @@ def gc_content_subsec(seq, k=20):
         subseq = seq[i:i+k]
         res.append(gc_content(subseq))
     return res
+
+def translate_seq(seq, init_pos=0):
+    """Translates a DNA sequence into an aminoacid sequence"""
+    return [DNA_Codons[seq[pos:pos+3]] for pos in range(init_pos, len(seq)-2,3)]
+
+def codon_usage(seq, aminoacid):
+    """Provides the frequency of each codon encoding a given aminoacid in a DNA sequence"""
+    tmpList = []
+    for i in range(0,len(seq) -2 ,3):
+        if DNA_Codons[seq[i:i+3]]==aminoacid:
+            tmpList.append(seq[i:i+3])
+
+    freqDict=dict(Counter(tmpList))
+    totalWight = sum(freqDict.values())
+    for seq in freqDict:
+        freqDict[seq] = round(freqDict[seq]/totalWight, 2)
+    return freqDict
