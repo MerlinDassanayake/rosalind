a3bb894b1813f3575bea7901721b986d25ea1b9c
Merlin Dassanayake <131916883+MerlinDassanayake@users.noreply.github.com>
2023-06-26 14:21:16 -0700

    open reading frames

--- main.py
+++ main.py
@@ -3,24 +3,23 @@ from DNAToolkit import *
 from utilities import colored
 import random
 
-rndDNAStr = ''.join([random.choice(Nucleotides)
-                     for nuc in range(50)])
+rndDNAStr = "".join([random.choice(Nucleotides) for nuc in range(50)])
 
 DNAStr = validateSeq(rndDNAStr)
 
-print(f'\nSequence: {colored(DNAStr)}\n')
-print(f'[1] + Sequence Length: {len(DNAStr)}\n')
-print(colored(f'[2] + Nucleotide Frequency: {countNucFrequency(DNAStr)}\n'))
-print(f'[3] + DNA/RNA Transcription: {colored(transcription(DNAStr))}\n')
+print(f"\nSequence: {colored(DNAStr)}\n")
+print(f"[1] + Sequence Length: {len(DNAStr)}\n")
+print(colored(f"[2] + Nucleotide Frequency: {countNucFrequency(DNAStr)}\n"))
+print(f"[3] + DNA/RNA Transcription: {colored(transcription(DNAStr))}\n")
 
 print(f"[4] + DNA String + reverse Complement:\n5' {colored(DNAStr)} 3' ")
 print(f"   {''.join(['|' for c in range(len(DNAStr))])}")
 print(f"3' {colored(reverse_complement(DNAStr)[::-1])} 5'\n")
 
-print(f'[5] + GC Content: {gc_content(DNAStr)}%\n')
-print(
-    f'[6] + GC Content in Subsection k=5: {gc_content_subsec(DNAStr, k=5)}\n')
-print(
-    f'[7] + Aminoacids Sequence from DNA: {translate_seq(DNAStr)}\n')
-print(
-    f'[8] + Codon frequency (L): {codon_usage(DNAStr, "L")}\n')
+print(f"[5] + GC Content: {gc_content(DNAStr)}%\n")
+print(f"[6] + GC Content in Subsection k=5: {gc_content_subsec(DNAStr, k=5)}\n")
+print(f"[7] + Aminoacids Sequence from DNA: {translate_seq(DNAStr)}\n")
+print(f'[8] + Codon frequency (L): {codon_usage(DNAStr, "L")}\n')
+print(f"[9] + Reading_frames: ")
+for frame in gen_reading_frames(DNAStr):
+    print(frame)
