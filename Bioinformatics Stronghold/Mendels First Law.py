7ee6215ae3bb3fea95a351c4c24ac74565fa0a53
MerlinDassanayake <dinushadassa@gmail.com>
2025-08-03 23:06:52 -0400

    mendel setup

---
+++ Bioinformatics Stronghold/Mendels First Law.py
@@ -0,0 +1,19 @@
+# Given k,m, and n as positive integers
+# representing population with k + m + n organisms.
+# k individuals are homozygous dominant for a factor.
+# m are heterozygous.
+# n are homozygous recessive.
+
+# Return: the probability that two randomly selected
+# mating organisms will produce an individual possessing a dominant allele
+# and thus displaying dominatnt phenotype
+# assume any two organisms can mate.
+
+
+k = int()
+m = int()
+n = int()
+
+population = k + m + n
+
+
