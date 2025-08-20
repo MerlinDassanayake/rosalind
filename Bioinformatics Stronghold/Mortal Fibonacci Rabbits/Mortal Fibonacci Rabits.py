68af1f93e4719bd38d99bb469e086f2cac92fa38
MerlinDassanayake <dinushadassa@gmail.com>
2025-08-19 21:14:14 -0400

    update mortal fib

--- Bioinformatics Stronghold/Mortal Fibonacci Rabbits/Mortal Fibonacci Rabits.py
+++ Bioinformatics Stronghold/Mortal Fibonacci Rabbits/Mortal Fibonacci Rabits.py
@@ -1,3 +1,7 @@
 # Given: Positive integers n <= 100 and m <= 20.
 # Return: The total number of pairs of rabbits that will remain after the n-th month if all rabbits live for m months.
 
+def mortal_rabbits(n, m):
+    previous1, previous2 = 1, 1
+    for i in range(2, n):
+        
